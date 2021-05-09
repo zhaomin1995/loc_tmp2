1a74c892fdb4edcd99c58f2cbf03f7858a80948a
xzm01234@gmail.com <zhaominxiao@my.unt.edu>
2021-05-08 19:04:07 -0500

    add comments for baseline scripts

--- baseline.py
+++ baseline.py
@@ -5,10 +5,17 @@ from sklearn.metrics import classification_report
 
 
 def main(data_dir):
+
     annotation_filename = "new_annot.json"
     annotation_dir = os.path.join(data_dir, 'annotations', annotation_filename)
+
+    # load data
     instances = preprocess.load_data(annotation_dir)
+
+    # add majority baseline output
     instances = preprocess.add_baseline_output(instances)
+
+    # make predication and evaluate
     gold_labels = [x['adjudicated_label'] for x in instances]
     pred_labels = [x['baseline_output'] for x in instances]
     print(classification_report(gold_labels, pred_labels))
