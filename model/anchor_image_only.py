955ccf5e6fbcd068b9078d83f7a6bee96cd00a18
xzm01234@gmail.com <zhaominxiao@my.unt.edu>
2021-05-09 19:30:48 -0500

    update the model initialization of anchor_text_only and anchor_image_only

---
+++ model/anchor_image_only.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+# parameter setting
+vgg_feat_dim = 1000
+output_dim = 2
+hidden_dim = 512
+dropout_rate = 0.2
+
+
+class AnchorImageOnlyModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.vgg_feat_dim = vgg_feat_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.dropout_rate = dropout_rate
+        self.fc1 = nn.Linear(self.vgg_feat_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, vgg_feat):
+        out = self.fc1(vgg_feat)
+        out = F.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        return out
---
+++ model/anchor_text_only.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+# parameter setting
+bert_feat_dim = 768
+output_dim = 2
+hidden_dim = 512
+dropout_rate = 0.2
+
+
+class AnchorTextOnlyModel(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.bert_feat_dim = bert_feat_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.dropout_rate = dropout_rate
+        self.fc1 = nn.Linear(self.bert_feat_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, bert_feat):
+        out = self.fc1(bert_feat)
+        out = F.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        return out
