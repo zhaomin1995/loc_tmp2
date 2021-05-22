147a6c01f597123db352580549f5460815890c7b
xzm01234@gmail.com <zhaominxiao@my.unt.edu>
2021-05-22 16:02:43 -0500

    optimize the script for baseline

--- baseline.py
+++ baseline.py
@@ -1,17 +1,16 @@
-import argparse
 import os
 from utils import preprocess
 from sklearn.metrics import classification_report
 
 
-def main(mode):
+def main():
 
     data_dir = 'data'
     annotation_filename = "new_annot.json"
     annotation_dir = os.path.join(data_dir, 'annotations', annotation_filename)
 
     # load data
-    instances = preprocess.load_data(annotation_dir, mode)
+    instances = preprocess.load_data(annotation_dir, mode='anchor_text_image')
 
     # add majority baseline output
     instances = preprocess.add_baseline_output(instances)
@@ -26,8 +25,5 @@ def main(mode):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-mode', '--mode', help='anchor_text_only or anchor_image_only or anchor_text_image')
-    args = parser.parse_args()
 
-    main(mode=args.mode)
+    main()
