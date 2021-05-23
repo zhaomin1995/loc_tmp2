37273a78a8eaa5a0541c26ab7c9f3728c38eee9b
xzm01234@gmail.com <zhaominxiao@my.unt.edu>
2021-05-22 23:03:24 -0500

    update the model for complicated_bert_only.py

---
+++ model/complicated_bert_only.py
@@ -0,0 +1,31 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+# parameter setting
+bert_feat_dim = 768
+output_dim = 2
+hidden_dim = 1024
+dropout_rate = 0.5
+
+
+class ComplicatedBertOnly(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.bert_feat_dim = bert_feat_dim
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        self.dropout_rate = dropout_rate
+        self.fc1 = nn.Linear(self.bert_feat_dim*7, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, feat_combined):
+
+        # pass fully-connected layer
+        out = self.fc1(feat_combined)
+        out = F.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        return out
